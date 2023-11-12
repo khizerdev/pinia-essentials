17d70f146e8089be94ecfa45814d00a6b3443440
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-11-13 00:37:23 +0500

    Mutating state with actions

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,7 +1,27 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { useTaskListStore } from "../stores/taskList";
+
+const task = ref("");
+const store = useTaskListStore();
+console.log(store.taskList);
+
+function addItemAndClear(item) {
+  if (item.length === 0) {
+    return;
+  }
+  // invokes function in the store:
+  store.addTask(item);
+  task.value = "";
+}
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <form @submit.prevent="addItemAndClear(task)">
+      <input v-model="task" type="text" /><button>Add</button>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
--- src/stores/taskList.js
+++ src/stores/taskList.js
@@ -3,5 +3,16 @@ import { defineStore } from "pinia";
 export const useTaskListStore = defineStore("taskList", {
   state: () => ({
     taskList: [],
+    id: 0,
   }),
+  actions: {
+    addTask(item) {
+      this.taskList.push({ item, id: this.id++, completed: false });
+    },
+    deleteTask(itemId) {
+      this.taskList = this.taskList.filter((item) => {
+        return item.id !== itemId;
+      });
+    },
+  },
 });
